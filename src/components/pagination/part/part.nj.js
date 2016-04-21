76fcaba0cb8b89aff770291c3e8b429e18afcba7
joe-sky <hitomi_doax@sina.com>
2016-04-21 19:57:29 +0800

    Update example

---
+++ src/components/pagination/part/part.nj.js
@@ -0,0 +1,9 @@
+﻿'use strict';
+
+let nj = require('nornj');
+
+module.exports = nj`
+<div>
+  测试1{id}测试799999
+</div>
+`;
--- src/components/pagination/template.nj.js
+++ src/components/pagination/template.nj.js
@@ -1,12 +1,16 @@
 ﻿'use strict';
 
-let nj = require('nornj');
+let nj = require('nornj'),
+  part = require('./part/part.nj');
 
 module.exports = nj`
 <div class=fj-pagn>
   <ul class=fj-pagn-body>
     <li class=fj-pagn-info>
-      共{id}页
+      ${part}
+    </li>
+    <li class=fj-pagn-info>
+      共{id}页88888
     </li>
     <li class=fj-pagn-btn-refresh>
       <i class="fa fa-refresh"></i>
